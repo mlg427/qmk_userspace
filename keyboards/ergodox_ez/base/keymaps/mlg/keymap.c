069fbd3e67a1b704b869bc8a2ebed21579459c69
mlg <micah@gwynne.id.au>
2026-04-30 21:55:56 +1000

    add numpad

--- keyboards/ergodox_ez/base/keymaps/mlg/keymap.c
+++ keyboards/ergodox_ez/base/keymaps/mlg/keymap.c
@@ -4,6 +4,7 @@
 enum layers {
     BASE,  // default layer
     GAME,  // gayming layer
+    NUM,   // numpad layer
     UTIL,  // symbols
 };
 
@@ -24,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| PREV |           |      |------+------+------+------+------+--------|
  * | LShift |  Z   |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | UTIL |      |      | LAlt |LSuper|                                       |RSuper|      |      |      |      |
+ *   | UTIL | NUM  |      | LAlt |LSuper|                                       |RSuper|      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
  *                                        |      |      |       |      |        |
@@ -40,12 +41,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,          KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    KC_MNXT,              KC_TRNS,      KC_Y,    KC_U,    KC_I,    KC_O,      KC_P,        KC_BSLS,
   CTL_T(KC_ESC),   KC_A,        KC_S,          KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L,      KC_SCLN,     KC_QUOT,
   KC_LSFT,         KC_Z,        KC_X,          KC_C,    KC_V,    KC_B,    KC_MPRV,              KC_TRNS,      KC_N,    KC_M,    KC_COMM, KC_DOT,    KC_SLSH,     KC_TRNS,
-  MO(UTIL),        KC_TRNS,     KC_TRNS,       KC_LALT, KC_LGUI,                                                       KC_RGUI, KC_TRNS, KC_TRNS,   KC_TRNS,     KC_TRNS,
+  MO(UTIL),        MO(NUM),     KC_TRNS,       KC_LALT, KC_LGUI,                                                       KC_RGUI, KC_TRNS, KC_TRNS,   KC_TRNS,     KC_TRNS,
                                                                  KC_TRNS, KC_TRNS,              KC_TRNS, KC_TRNS,
                                                                           KC_TRNS,              KC_TRNS,
                                                          KC_SPC, KC_BSPC, KC_TRNS,              KC_TRNS, KC_ENT, KC_SPC
 ),
-/* Keymap 2: epic layer
+/* Keymap 3: epic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * | FLASH  | F1   | F2   | F3   | F4   | F5   |      |           |      | F6   | F7   | F8   | F9   | F10  | F11    |
@@ -80,6 +81,39 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  ),
 
 
+/* Keymap 2: numpad layer
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |        |      |      |      |      |      |      |           |      |      |NumLoc|  /   |  *   |  -   |        |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * |        |      |      |      |      |      |      |           |      |      |   7  |  8   |  9   |  +   |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |      |      |      |      |      |------|           |------|      |  4   |  5   |  6   |  +   |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |      |      |      |      |      |      |           |      |      |  1   |  2   |  3   |Enter |        |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |      |      |      |      |      |                                       |  0   |  0   |  .   |Enter |      |
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        |      |      |       |      |      |
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      |      |       |      |      |      |
+ *                                 |      |      |------|       |------|      |      |
+ *                                 |      |      |      |       |      |      |      |
+ *                                 `--------------------'       `--------------------'
+ */
+[NUM] = LAYOUT_ergodox_pretty(
+ 
+   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS, KC_TRNS,
+   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_KP_7, KC_KP_8, KC_KP_9, KC_PPLS, KC_TRNS,
+   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                       KC_TRNS, KC_KP_4, KC_KP_5, KC_KP_6, KC_PPLS, KC_TRNS,
+   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_KP_1, KC_KP_2, KC_KP_3, KC_PENT, KC_TRNS,
+   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                         KC_KP_0, KC_KP_0, KC_PDOT, KC_PENT, KC_TRNS,
+ 
+                                                KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,
+                                                         KC_TRNS,     KC_TRNS,
+                                       KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS
+ ),
 
 
 
